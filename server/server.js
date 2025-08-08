74798670cabbb92e72ad63f27c46e9d4fbf438ac
Sanskriti Uniyal <elvaelsa588@gmail.com>
2025-08-09 03:52:34 +0530

    � Updated API URL for live backend as well as server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: 'https://promtap.onrender.com' }));
 app.use(express.json());
 
 // Prefix all auth routes with /api/auth
